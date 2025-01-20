30f421c9a8d4d619ca77caa1c5d820f5ae544b62
Ilia Lazarev <kephircheek@gmail.com>
2025-01-20 12:24:01 +0300

    Позволяет считать эволюцию тюнинга в пространстве Лиувилля

--- orrvciwe.py
+++ orrvciwe.py
@@ -9,6 +9,7 @@ import numpy as np
 import sympy as sp
 import timeti
 from scipy import optimize
+from scipy import linalg
 from quanty import matrix
 from quanty.base import sz
 from quanty.problem.transfer import TransferZQCAlongChain
@@ -59,6 +60,34 @@ def dephasing_values(gammas, ex):
     return values
 
 
+def indentity_like(mat):
+    return np.eye(mat.shape[0])
+
+
+def left_superoperator(mat):
+    indentity = indentity_like(mat)
+    return np.kron(indentity, mat)
+
+
+def right_superoperator(mat):
+    indentity = indentity_like(mat)
+    return np.kron(mat.transpose(), indentity)
+
+
+def dephasing_superoperator(gammas, ex):
+    values = 0
+    n = len(gammas)
+    for i, gamma in enumerate(gammas):
+        if gamma == 0:
+            continue
+        sz_ = matrix.crop(sz(n, i).toarray(), ex=ex).astype(float) / 2
+        _values = np.kron(sz_, sz_)  # left_superoperator(sz_) @ right_superoperator(sz_)
+        _values -= indentity_like(_values) * 0.25
+        _values = _values * gamma
+        values += _values
+    return values
+
+
 @functools.cache
 def dephasing_operator(t, gammas, ex):
     values = dephasing_values(gammas, ex=ex)
@@ -79,7 +108,11 @@ class TransferTask(TransferZQCPerfectlyTask):
     nt_per_step: int = 3
 
     def tune(self, state):
-        # return state
+        if self.nt_per_step is None:
+            return self.tune_in_liouville_space(state)
+        return self.tune_by_trotterization(state)
+
+    def tune_by_trotterization(self, state):
         nt = self.nt_per_step * len(self.features)
         tt = self.tuning_time / nt
         u = self.problem.hamiltonian.U(self.problem.length, tt, ex=self.problem.ex)
@@ -92,6 +125,20 @@ class TransferTask(TransferZQCPerfectlyTask):
             state = state * ul
             state = u @ state @ u.conjugate().transpose()
             t += tt
+        return state
+
+    def tune_in_liouville_space(self, state):
+        n_state_rows = state.shape[0]
+        state = state.transpose().flatten().reshape(-1, 1)
+        ham = self.problem.hamiltonian(self.problem.length, ex=self.problem.ex)
+        super_ham = left_superoperator(ham) - right_superoperator(ham)
+        dt = self.tuning_time / len(self.features)
+        for gammas in self.features:
+            super_do = dephasing_superoperator(gammas, ex=self.problem.ex)
+            superoperator = super_ham + 1j * super_do
+            u = linalg.expm(-1j * superoperator * dt)
+            state = u @ state
+        state = state.reshape(n_state_rows, -1).T
         return state
 
     def _residuals(self, var, impact):
